808db9dc45b380251afa54f704a5217eab3c8d03
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2025-05-26 15:59:51 -0400

    Add files via upload

---
+++ dac/bytegreen/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class Funcionario(AbstractUser):
+    email = models.EmailField(unique=True)
+    setor = models.ForeignKey('Setor', on_delete=models.SET_NULL, null=True)
+    tipo = models.CharField(max_length=20, choises=[
+        ('médico','Médico'),
+        ('chefe','Chefe de Setor'),
+        ('gerente', 'Gerente'),
+        ('funcionario', 'Funcionario')
+    ])
+
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    
