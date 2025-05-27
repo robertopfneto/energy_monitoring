ecd949ad2f43724d86f43dbc6f33c4492f2fd8f2
robertopfneto <neto2014roberto@gmail.com>
2025-05-27 10:55:24 -0300

    adicionando hospital a previsao e definindo rotas

--- dac/bytegreen/urls.py
+++ dac/bytegreen/urls.py
@@ -1,5 +1,7 @@
 from rest_framework import routers
 from django.urls import path, include
+
+from bytegreen.predictive_model.predict_view import PredictAPIView, PredictHospitalAPIView
 from .views import *
 
 router = routers.DefaultRouter()
@@ -17,4 +19,7 @@ router.register(r'leituras_hospital', LeituraHospitalViewSet)
 
 urlpatterns = [
     path('', include(router.urls)), 
+     path('predict/setor/<int:setor_id>/', PredictAPIView.as_view(), name='predict-setor'),
+    path('predict/hospital/<int:hospital_id>/', PredictHospitalAPIView.as_view(), name='predict-hospital'),
+   
 ]
